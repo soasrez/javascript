ff81c24fa1d07d2c39068ac33187b41304649045
soasrez <soasres@outlook.com>
2024-10-21 15:50:23 -0300

    ex008

---
+++ aula11/ex008.js
@@ -0,0 +1,16 @@
+console.log('funcionou!')
+
+
+var vel = 58
+
+console.log(`A velocidade do seu carro é de ${vel}KM/h`)
+
+if (vel > 60) { //CONDIÇÃO SIMPLES (PORQUE NAO TEM ELSE)
+    console.log(`Você ultrapassou a velocidade permitida, foi MULTADO!`)
+}
+
+console.log(`Dirija sempre com segurança.`)
+
+
+
+
