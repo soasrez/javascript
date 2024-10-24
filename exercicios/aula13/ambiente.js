8f1db1fd84df5d27c2dd26b65fa12cf21e435efb
soasrez <soasres@outlook.com>
2024-10-24 17:29:54 -0300

    AULA13

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,32 @@
+// Estrutura de Repetição com Teste Lógico no Final (DO WHILE)
+
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)
+
+// Estrutura de Repetição com Teste Lógico no Início (WHILE)
+var c = 1
+while (c <= 500) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1 / c recebe c + 1
+}
+
+
+
+
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
