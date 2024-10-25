2f514e1c47cfc8e9ca3aa4287bb8c462d370159f
soasrez <soasres@outlook.com>
2024-10-25 16:51:59 -0300

    aula16

---
+++ exercicios/aula16/funcao04.js
@@ -0,0 +1,13 @@
+function fatorial(n) {
+    let fat =1
+    for(let c=n; c > 1; c--) {
+        fat *= c
+    }
+    return fat
+}
+
+console.log(fatorial(5))
+
+
+// 5! = 5 x 4 x 3 x 2 x 1 = Fatorial de 5 é = 120
+// 5 Fatorial
