d6d4bc4e96e9cf594c540b638743075ad2ca0599
soasrez <soasres@outlook.com>
2025-01-24 16:16:39 -0300

    added function removerItem

--- template_cart/js/cartmkt.js
+++ template_cart/js/cartmkt.js
@@ -11,3 +11,14 @@ function adicionarItem(item){
     quantidade.innerHTML = qtd[item];
     total.innerHTML = valorTotal[item].toFixed(2);
 }
+
+function removerItem(item){
+    if (qtd[item] > 0){
+        qtd[item] -= 1;
+        var quantidade = document.getElementById('quantidade' + item);
+        var total = document.getElementById('total' + item);
+        valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
+        quantidade.innerHTML = qtd[item];
+        total.innerHTML = valorTotal[item].toFixed(2);
+    }
+}
