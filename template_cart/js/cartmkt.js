091a188565d106693a38da3cdd5c376e015dfe57
soasrez <soasres@outlook.com>
2025-01-24 16:06:57 -0300

    adding function adicionarItem

---
+++ template_cart/js/cartmkt.js
@@ -0,0 +1,13 @@
+var valorTotal = [0,0];
+var valorProduto = [50.00,30.00];
+var qtd = [0,0];
+
+function adicionarItem(item){
+
+    var quantidade = document.getElementById('quantidade0' + item);
+    var total = document.getElementById('total' + item);
+    qtd[item] += 1;
+    valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
+    quantidade.innerHTML = qtd[item];
+    total.innerHTML = valorTotal[item].toFixed(2);
+}
