5a9c2d1aa6066a15652b087b7f47abf01a9de34b
soasrez <soasres@outlook.com>
2025-03-12 18:16:20 -0300

    Create switch.js

---
+++ exercicios/aula04/switch.js
@@ -0,0 +1,35 @@
+let valor = "água"
+
+
+
+switch(valor){
+
+ 
+
+case "café":
+
+    console.log("O café custa R$2.50")
+
+    break;
+
+  case "leite":
+
+    console.log("O leite custa R$3.00")
+
+    break;
+
+ 
+
+  case "chá":
+
+    console.log("O chá custa R$2.00")
+
+    break;
+
+
+
+  default:
+
+    console.log("Sinto muito, mas no momento só temos café, leite ou chá.")
+
+}
